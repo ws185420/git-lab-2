581217901f50329b6fb01dbf14bcab0fd4c05eda
William Smith <ws185420@ohio.edu>
2022-01-21 15:39:10 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: William Smith
+ *        Date: 01/21/2022
+ * Description: git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
